bb9d97155a7a9c6767f3d2923b81ff5293a9f2e3
Heyrin <77548431+devheyrin@users.noreply.github.com>
2022-03-31 18:34:30 +0900

    ADD week1.cpp

---
+++ week1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int age = 20;
+    double height = 185;
+    // 문자열 두 개를 이어서 출력하기 위해 변수를 두 개 선언한다. 
+    char bloodType1 = 'A';
+    char bloodType2 = 'B';
+    printf("My name is heyrin\n");
+    printf("I'm %d years old\n", age);
+    printf("My blood type is %c%c\n", bloodType1, bloodType2);
+}
